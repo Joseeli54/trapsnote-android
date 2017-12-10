89972e3cec562c5746da5812ba084091962c5fbf
JoseEli54 <barrientosjoseelias@hotmail.com>
2017-12-10 00:31:15 -0400

    android-3
    Se genera el formulario de inicio de sesion y obtiene los datos.

---
+++ app/src/main/java/com/exam/sid/aplicacion/Task.java
@@ -0,0 +1,18 @@
+package com.exam.sid.aplicacion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Elias Barrientos on 12/5/2017.
+ */
+
+public class Task extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task);
+    }
+
+
+}
